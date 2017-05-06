389b24d93a472ee18cfbbe3cafb161be8ca19ffb
Juan Diego Jimenez <jdiejim@gmail.com>
2017-05-06 11:10:08 -0600

    add style, fonts, new elements

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.css$/, loader: "style!css" }
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
     ]
   },
   resolve: {
